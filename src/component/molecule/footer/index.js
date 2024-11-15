a075c533305da6a2a93df158d93dbaa249d7ba8c
Howard2510 <S22210550@student.unklab.ac.id>
2024-11-15 15:36:48 +0800

    adding footer component

--- src/component/molecule/footer/index.js
+++ src/component/molecule/footer/index.js
@@ -1,8 +1,22 @@
+
+import React from 'react';
+import '../../../assets/css/style.css'
+
 const Footer = () => {
   return (
-    <div>
-      <h3>Footer</h3>
-    </div>
+    <footer className="footer">
+      <p>&copy; 2024 OtoRelax Car Salon. All rights reserved.</p>
+      <p>
+        <a href="#about">About Us</a> | 
+        <a href="#services">Services</a> | 
+        <a href="#contact">Contact</a>
+      </p>
+      <div className="social-icons">
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+      </div>
+    </footer>
   );
 };
 
