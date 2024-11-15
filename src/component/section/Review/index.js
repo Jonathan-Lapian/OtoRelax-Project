73e20d3f204dc1b76e70f3754886991dd4d47888
Howard2510 <S22210550@student.unklab.ac.id>
2024-11-15 17:18:28 +0800

    test

--- src/component/section/Review/index.js
+++ src/component/section/Review/index.js
@@ -1,7 +1,32 @@
+import React from 'react';
+import './ReviewContainer.css';
+import ProfileImage from '../../../assets/img/Profil.jpeg'; // Import the image
+
+const reviews = [
+  {
+    name: 'Sarah',
+    title: 'Arsitek, Manado',
+    comment: 'Berkat Pelayanan Otorelax Mobil saya menjadi kinclong dan bersih jadi seperti mobil baru lagi, mana pelayanannnya bagus lagi',
+    image: ProfileImage // Use the imported image
+  },
+];
+
 const Review = () => {
   return (
-    <div>
-      <h2>Review</h2>
+    <div className="review-container">
+      <h2>Kata mereka yang telah menggunakan OtoRelax</h2>
+      <div className="reviews">
+        {reviews.map((review, index) => (
+          <div className="review-card" key={index}>
+            <div className="review-content">
+              <img src={review.image} alt={`${review.name}`} className="review-image" />
+              <p className="review-comment">"{review.comment}"</p>
+              <p className="review-author">{review.name}</p>
+              <p className="review-title">{review.title}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
